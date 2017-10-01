7e3cb012c176022ab2b060738d8003aee8888a88
stylobie <spserban@gmail.com>
2017-10-01 12:25:16 +0200

    calcul ex 2

--- calcul.py
+++ calcul.py
@@ -1,5 +1,22 @@
-print((2*3+9)/5)
+from math import *
 
-print(31//5)
+if 1==1 :
+    print((2 * 3 + 9) / 5)
 
-print(31%5)
+    print(31 // 5)
+
+    print(31 % 5)
+
+    print(31 / 5)
+
+    print(2**3)
+
+    print(pow(0.5, 3))
+
+    print(sqrt(144))
+
+    print(sin(pi/3))
+
+print(3+7==2*5)
+
+print(4-1==-1)
