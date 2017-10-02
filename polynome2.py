b8afb12b200dfcae2d9e791124cccbdc0ef4ad82
stylobie <spserban@gmail.com>
2017-10-02 22:46:31 +0200

    #

---
+++ polynome2.py
@@ -0,0 +1,17 @@
+from math import *
+a = input("que vaut a, coeff de x^2")
+b = input("que vaut b, coeff de x")
+c = input("que vaut b?")
+b**2 * 4 a c = delta
+(-b / 2 a) = x0
+(-b + sqrt(delta)) / (2 a) = x1
+(-b - sqrt(delta)) / (2 a) = x2
+
+
+if delta < 0:
+    print("le polynome n'admet pas de racines réeles dans R")
+elif delta = 0
+    print("le polynome admet une racine double sur R : x =", x0)
+else delta > 0
+    print("le polynome admet deux racines sur R : x1 :", x1, "et x2 :", x2)
+
